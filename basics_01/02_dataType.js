ffa82ab05641253214ca945aaaf5b6f4fe2284dc
Osama <126106612+osamashams030@users.noreply.github.com>
2026-04-13 08:45:41 +0000

    constraint config done

---
+++ basics_01/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as newer version
+// alert(3 + 3) we are using nodjs, not browser
+
+let age = 23
+let isLoggedIn = false
+let state = null;
+
+// number=> 2 to power of 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Akib Jawed"); // define which type of data
+console.log(typeof null);  // object
+console.log(typeof umdefined); // umdefined
+
+
