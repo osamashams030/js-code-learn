0845fa7ebe582e5a7e489e0be530712d4fcf3a1c
Osama <126106612+osamashams030@users.noreply.github.com>
2026-04-28 23:39:24 +0000

    Maths and Nums

---
+++ basics_01/07_nums_maths.js
@@ -0,0 +1,45 @@
+const score = 400
+console.log(score);
+const balance = new Number(300)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 23.4535
+console.log(otherNumber.toPrecision(3));
+
+const num = 10000000
+console.log(num.toLocaleString());
+console.log(num.toLocaleString('en-IN'));
+
+// ======================== Maths =======================
+
+console.log(Math)
+console.log(Math.abs(-5));
+console.log(Math.abs(5));
+console.log(Math.round(4.45));
+console.log(Math.ceil(7.65));
+console.log(Math.floor(4.554));
+
+console.log(Math.min(3, 4, 1, 7, 9, 32, 45));
+console.log(Math.max(3, 4, 1, 7, 9, 32, 45));
+
+console.log(Math.random());
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
